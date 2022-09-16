23922cffe869bdae2151e906e4eb46ddc0c9c41e
Claudio Assis <96958858+Cl84Dev@users.noreply.github.com>
2022-09-16 16:25:26 -0300

    Add Zerar Placar button

--- jokenpo.js
+++ jokenpo.js
@@ -6,6 +6,8 @@ function jokenpo(){
 
 let cpu = parseInt(Math.random() * 3)
 let player = parseInt(document.getElementById('player').value)
+let reset = document.createElement('button')
+reset.id = 'reset'
 let cpuChoice
 
     if (cpu === 0) {
@@ -20,20 +22,44 @@ let cpuChoice
         document.getElementById('cpuChoice').innerHTML = cpuChoice
         document.getElementById('result').innerHTML = 'Empatou! &#128556;'
         tie = tie + 1
-        document.getElementById('score').innerHTML = `Vitórias: ${win}; Empates: ${tie}; Derrotas: ${lose}`
-        
+
     } else if (player === 0 && cpu === 2 || player === 1 && cpu === 0 || player === 2 && cpu === 1 ) {
         document.getElementById('cpuChoice').innerHTML = cpuChoice
         document.getElementById('result').innerHTML = 'Você ganhou! &#128515;'
         win = win + 1
-        document.getElementById('score').innerHTML = `Vitórias: ${win}; Empates: ${tie}; Derrotas: ${lose}`
 
     } else {
         document.getElementById('cpuChoice').innerHTML = cpuChoice
         document.getElementById('result').innerHTML = 'Você perdeu! &#128557;'
         lose = lose + 1
-        document.getElementById('score').innerHTML = `Vitórias: ${win}; Empates: ${tie}; Derrotas: ${lose}`
-
+        
     }
-    
+
+    document.getElementById('score').innerHTML = `Vitórias: ${win}  Empates: ${tie}  Derrotas: ${lose}`
+}
+
+function reset() {
+    win = 0
+    tie = 0
+    lose = 0
+    document.getElementById('score').innerHTML = `Vitórias: ${win}  Empates: ${tie}  Derrotas: ${lose}`
+}
+
+function jokenpoPlay() {
+    document.getElementById('cpuChoice').innerHTML = ''
+    document.getElementById('result').innerHTML = ''
+    setTimeout(jo, 500)
+    setTimeout(ken, 1000)
+    setTimeout(po, 1500)
+    setTimeout(jokenpo, 2000)
+}
+
+function jo() {
+    document.getElementById('cpuChoice').innerHTML ='JO'
+}
+function ken() {
+    document.getElementById('cpuChoice').innerHTML ='KEN'
+}
+function po() {
+    document.getElementById('cpuChoice').innerHTML ='PO!'
 }
